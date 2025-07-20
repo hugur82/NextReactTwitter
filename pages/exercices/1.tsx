fe0c2673305c74029b8c6b40e72676c2fcbb9316
khuq <ugur_hakan@yahoo.fr>
2025-07-20 12:57:24 +0200

    finish exercice 1

--- pages/exercices/1.tsx
+++ pages/exercices/1.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
+import z from 'zod';
 import { Loader } from '~/components/Loader';
 import type { TlTweets } from '~/lib/scheme/tweets';
 import { AddTweetForm } from '../../src/components/tweets/AddTweetForm';
@@ -10,28 +11,105 @@ import TwitterLayout from '../../src/components/TwitterLayout';
 
 const notifyFailed = () => toast.error("Couldn't fetch tweet...");
 
-// 🦁 Créer un schéma zod appelé TweetsScheme qui correspond à la réponse de l'API
-// Tu peux `console.log` la réponse de l'API pour voir la structure attendue
-// Tu pourrais utiliser zod transform pour modifier directement dans le schéma la date
-// 💡 const TweetsScheme = z.object({...
+const TweetsScheme = z.object({
+  tweets: z.array(
+    z.object({
+      id: z.string(),
+      content: z.string(),
+      createdAt: z.string(),
+      user: z.object({
+        id: z.string(),
+        displayName: z.string(),
+        username: z.string(),
+        avatarUrl: z.string(),
+      }),
+      _count: z.object({
+        replies: z.number(),
+        likes: z.number(),
+      }),
+      liked: z.boolean(),
+    })
+  ),
+});
+
+export type ClientConfig<T> = {
+  data?: unknown;
+  zodSchema?: z.ZodSchema<T>;
+  method?: 'DELETE' | 'GET' | 'OPTIONS' | 'PATCH' | 'POST' | 'PUT';
+  headers?: HeadersInit;
+  // Pour pouvoir override la config
+  customConfig?: RequestInit;
+  signal?: AbortSignal;
+};
+
+export async function client<T>(
+  url: string,
+  {
+    data,
+    zodSchema,
+    method,
+    headers: customHeaders,
+    signal,
+    customConfig,
+  }: ClientConfig<T> = {}
+): Promise<T> {
+  const config: RequestInit = {
+    method: method ?? (data ? 'POST' : 'GET'),
+    // On stringify les données s'il y en a
+    body: data ? JSON.stringify(data) : null,
+    headers: {
+      'Content-Type': data ? 'application/json' : '',
+      Accept: 'application/json',
+      // Mais on laisse l'utilisateur override les headers
+      ...customHeaders,
+    },
+    signal,
+    // On laisse l'utilisateur override la config
+    // S'il passe body, method, headers, etc... on les écrasera
+    ...customConfig,
+  };
+
+  return window.fetch(url, config).then(async (response) => {
+    // on gère le status 401 en arrêtant directement la request
+    if (response.status === 401) {
+      return Promise.reject(new Error("You're not authenticated"));
+    }
+
+    let result = null;
+    // 🦁 à toi de parse le json dans un try catch
+    try {
+      result = response.status === 204 ? null : await response.json();
+    } catch (error: unknown) {
+      return Promise.reject(error);
+    }
+    if (response.ok) {
+      return zodSchema && result ? zodSchema.parse(result) : result;
+    } else {
+      return Promise.reject(result);
+    }
+  });
+}
+
+const getTweets = async (signal: AbortSignal) =>
+  client('/api/tweets?error=test', { signal, zodSchema: TweetsScheme }); // ℹ️ tu peux remplacer l'url par `/api/tweets?error=erreur` pour voir le problème
 
 export default function FetchAllTweets() {
   const [tweets, setTweets] = useState<TlTweets | null>(null);
 
   useEffect(() => {
-    // 🦁 Créer un abort controller pour annuler la requête si l'utilisateur quitte la page
-
-    // 🦁 Passer le signal à la requête fetch
-    fetch('/api/tweets') // ℹ️ tu peux remplacer l'url par `/api/tweets?error=erreur` pour voir le problème
-      .then((res) => res.json())
+    const abortController = new AbortController();
+    getTweets(abortController.signal)
       .then((data) => {
-        // 🦁 Utiliser le schéma TweetsScheme pour valider la réponse de l'API
-
         setTweets(data.tweets);
       })
-      .catch(() => void 0); // 🦁 Ajouter un catch pour gérer les erreurs
+      .catch((err) => {
+        if (err.name === 'AbortError') return;
+        notifyFailed();
+      });
 
-    // 🦁 Créer la cleanup fonction qui annule la requête
+    return () => {
+      abortController.abort();
+    };
   }, []);
 
   if (!tweets) return <Loader />;
